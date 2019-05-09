d0263c08ea57a60fe884742f5e191fc1c9c792a1
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-09 16:10:16 -0600

    WIP most of lambda-classes works

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,88 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes) {
+        this.age = attributes.age;
+        this.name = attributes.name;
+        this.location = attributes.location;
+    }
+    speak() {
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes)
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student, object) {
+        console.log(`${Student.name} recieves a perfect score on ${subject}`)
+    }
+}
+
+class Student extends Person {
+    constructor(attributes) {
+        super(attributes)
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;
+    }
+    listsSubjects() {
+        console.log(steve.favSubjects);
+    }
+    PRAssignment(subject) {
+        console.log(`${Student.name} has submitted a PR for ${subject}`)
+    }
+    sprintChallenge(subject) {
+        console.log(`${Student.name} has begun sprint challenge on ${subject}`)
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(attributes) {
+        super(attributes)
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+    standUp(channel) {
+        console.log(`${ProjectManager.name} announces to ${channel}, @channel standy times!`);
+    }
+    dubugsCode(student, subject) {
+        console.log(`${ProjectManager.name} debugs ${Student.name}'s code on ${subject}`)
+    }
+}
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+const steve = new Student({
+  name: 'Steve',
+  location: 'Atown',
+  age: 12,
+  previousBackground: 'meme chef',
+  className: 'web200',
+  favSubjects: `not javascript`
+});
+
+const karen = new ProjectManager({
+  name: 'Karen',
+  location: 'Bikini Bottom',
+  age: 56,
+  gradClassName: 'web4',
+  favInstructor: 'Fred',
+});
+
+karen.standUp();
+steve.listsSubjects();
+fred.demo();
